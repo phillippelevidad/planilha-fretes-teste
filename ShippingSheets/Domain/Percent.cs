24a4feb6a7607a413e302cd8e3a8c0ca61535236
Phillippe Santana <phillippelevidad@gmail.com>
2020-06-29 14:24:12 -0300

    Updated debugger display for the Percent type.

--- ShippingSheets/Domain/Percent.cs
+++ ShippingSheets/Domain/Percent.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace ShippingSheets.Domain
 {
-    [DebuggerDisplay("{Value}%")]
+    [DebuggerDisplay("{Value}")]
     public class Percent : ValueObject<Percent>
     {
         public static readonly Percent Zero = new Percent(0);
